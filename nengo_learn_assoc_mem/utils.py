8566fe71f79ec84c1490f79120430a54a44e00fe
Sean Aubin <seanaubin@gmail.com>
2018-01-23 14:37:21 -0500

    feedback alignment continues to fail

--- nengo_learn_assoc_mem/utils.py
+++ nengo_learn_assoc_mem/utils.py
@@ -10,6 +10,12 @@ from typing import Sequence, List, Tuple
 dt = 0.001
 
 
+def scale_array(x, out_range=(-1, 1), axis=None):
+    domain = np.min(x, axis), np.max(x, axis)
+    y = (x - (domain[1] + domain[0]) / 2) / (domain[1] - domain[0])
+    return y * (out_range[1] - out_range[0]) + (out_range[1] + out_range[0]) / 2
+
+
 def list_as_ascii(li: List[str]):
     return [l.encode("ascii", "ignore") for l in li]
 
