8db4d640830fda026d6cbabd286da6ec01340a83
Sean Aubin <seanaubin@gmail.com>
2018-04-18 16:51:36 -0700

    better results for voja rec

--- experiments/run_neg_voja_meg.py
+++
@@ -1,9 +0,0 @@
-n_items = 16
-dimensions = 32
-n_neurons = 500
-
-dt = 0.001
-seed = 8
-
-vocab, fan1, fan1_pair_vecs, fan2, fan2_pair_vecs, foil1, foil1_pair_vecs, foil2, foil2_pair_vecs = make_alt_vocab(n_items, n_items, dimensions, seed, norm=True)
-
