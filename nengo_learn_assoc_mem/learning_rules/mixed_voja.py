d2c7648a276c5e7f757d37372012c96c85455d6f
Sean Aubin <seanaubin@gmail.com>
2018-08-18 14:17:27 -0400

    added dot pro learning rule

--- nengo_learn_assoc_mem/learning_rules/mixed_voja.py
+++ nengo_learn_assoc_mem/learning_rules/mixed_voja.py
@@ -64,3 +64,34 @@ class MeanMixed(FakeVoja):
             self.encoder_hist.append(self.encoders.copy())
 
         return np.dot(self.encoders, self.in_sig)
+
+
+class DotMixed(FakeVoja):
+
+    def __init__(self, encoders: np.ndarray, max_rates: np.ndarray, thresh: float, max_dist=0.2,
+                 post_tau=0.005, learning_rate=1e-3, radius=1., sample_every=0.1):
+        super().__init__(encoders, post_tau, learning_rate, sample_every)
+        self.radius = radius
+        self.thresh = thresh
+        self.max_dist = max_dist
+        self.max_rates = max_rates
+
+    def encode(self, t):
+        firing_ratio = self.acts / self.max_rates
+
+        lr = self.enabled * self.learning_rate
+
+        dist = self.encoders - self.in_sig
+        dist_mag = np.dot(self.encoders, self.in_sig)
+        dist[dist_mag > self.max_dist] = 0.
+
+        delta = lr * (firing_ratio[:, None] - self.thresh) * dist
+
+        mod_enc = self.encoders + delta
+        mag = np.linalg.norm(mod_enc, axis=1)
+        self.encoders = self.radius / mag[:, None] * mod_enc
+
+        if (t / self.dt % self.period) < 1:
+            self.encoder_hist.append(self.encoders.copy())
+
+        return np.dot(self.encoders, self.in_sig)
--- nengo_learn_assoc_mem/learning_rules/tests/test_mixed_voja.py
+++ nengo_learn_assoc_mem/learning_rules/tests/test_mixed_voja.py
@@ -1,7 +1,7 @@
 import nengo
 import numpy as np
 
-from nengo_learn_assoc_mem.learning_rules.mixed_voja import MeanMixed
+from nengo_learn_assoc_mem.learning_rules.mixed_voja import DotMixed
 
 seed = 8
 dimensions = 2
@@ -9,14 +9,15 @@ n_neurons = 50
 
 x_val = np.linspace(0, 2*np.pi, n_neurons)
 enc = np.array([np.cos(x_val), np.sin(x_val)]).T
+max_rates = np.ones(n_neurons) * 150
 
 with nengo.Network() as model:
     in_nd = nengo.Node([1, 1])
     nvoja_learn = nengo.Node(1)
 
-    ens = nengo.Ensemble(n_neurons, dimensions, seed=seed)
+    ens = nengo.Ensemble(n_neurons, dimensions, max_rates=max_rates, seed=seed)
 
-    neg_voja = MeanMixed(enc)
+    neg_voja = DotMixed(enc, max_rates, 0.1)
 
     nengo.Connection(in_nd, neg_voja.input_signal, synapse=None)
     nengo.Connection(ens.neurons, neg_voja.input_activities, synapse=0)
